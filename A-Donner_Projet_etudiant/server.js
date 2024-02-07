a4f70f82f9edb9e5fae997470d463072facd4f01
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-02-07 15:59:52 -0500

    Afficher les évènements + revenir à l'accueil

--- A-Donner_Projet_etudiant/server.js
+++ A-Donner_Projet_etudiant/server.js
@@ -1,112 +1,96 @@
-/*
-Importation des modules requis
-*/
-//import express from "express";
-//import session from "express-session";
-//import path from "path";
-//import { fileURLToPath } from "url";
-//import mysql from "mysql";
-//import { body, validationResult } from "express-validator";
-//import dateFormat from "dateformat";
+import express from "express";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+import mysql from "mysql";
+import { body, validationResult } from "express-validator";
+import dateFormat from "dateformat";
+import bodyParser from "body-parser";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-let express = require("express");
-let http = require("http");
-let mysql = require("mysql"); 
-let app = express();
-let bodyParser = require("body-parser");
-//const app = express();
-//const __filename = fileURLToPath(import.meta.url);
-//const __dirname = path.dirname(__filename);
-/*
-Connect to server
-*/
-const server = app.listen(4000, function() {
-console.log("serveur fonctionne sur 4000... ! ");
-});
+const app = express();
 
-/*
-Configuration de EJS
-*/
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-/*
-Importation de Bootstrap
-*/
-app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
-app.use(express.static('public'));
-
-
-/*
-Connection au server MySQL
-*/
+// Connect to server MySQL
 const con = mysql.createConnection({
     host: "localhost",
     user: "scott",
     password: "oracle",
     database: "mybd"
 });
+
 con.connect(function(err) {
     if (err) throw err;
-    console.log("connected!");
+    console.log("Connected to MySQL!");
 });
 
+app.use(bodyParser.urlencoded({extended:true}))
 
-/*
-Description des routes
-*/
+// Configuration EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Import Bootstrap
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+app.use(express.static('public'));
+
+// Routes
 app.get("/", function (req, res) {
-    con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err,
-result) {
+    con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err, result) {
         if (err) throw err;
         res.render("pages/index", {
             siteTitle: "Application simple",
             pageTitle: "Liste d'événements",
-            items: result
+            items: result, 
+            dateFormat : dateFormat
         });
     });
 });
-
 
 app.get("/event/add", function (req, res) {
-    con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err, 
-result) {
-        if (err) throw err;
-        res.render("pages/add-event", {
-            siteTitle: "Application simple",
-            pageTitle: "Ajouter un nouvel événement",
-            items: result
-        });
-    });
-});
-    
-
-app.post("/event/add", function (req, res) {
-    const requete = "INSERT INTO e_events (e_name, e_start_date, e_start_end, e_desc, e_location) VALUES (?, ?, ?, ?, ?)";
-    const parametres = [
-    req.body.e_name,
-    dateFormat(req.body.e_start_date, "yyyy-mm-dd"),
-    dateFormat(req.body.e_start_end, "yyyy-mm-dd"),
-    req.body.e_desc,
-    req.body.e_location
-    ];
-
-    con.query(requete, parametres, function (err, result) {
-        res.redirect("pages/index");
+    con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err,
+    result) {
     if (err) throw err;
-    res.redirect("pages/index");
+    res.render("pages/add-event", {
+    siteTitle: "Application simple",
+    pageTitle: "Ajouter un nouvel événement",
+    items: result
     });
     });
+    });
+
+
+
+
+
+
+
+// Start server
+const server = app.listen(4000, function() {
+    console.log("Le serveur fonctionne sur le port 4000 !");
+});
+
+    app.post("/event/add", function (req, res) {
+
+
+        const requete = "INSERT INTO e_events (e_name, e_start_date, e_start_end, e_desc, e_location) VALUES (?, ?, ?, ?, ?)";
+        const parametres = [
+            req.body.e_name,
+            dateFormat(req.body.e_start_date, "yyyy-mm-dd"),
+            dateFormat(req.body.e_start_end, "yyyy-mm-dd"),
+            req.body.e_desc,
+            req.body.e_location
+        ];
     
-    /*
-    Permettre l'utilisation de body lors des POST request
-    */
+        con.query(requete, parametres, function (err, result) {
+            if (err) throw err;
+            res.redirect("/");
+        });
+    
+        
+    });
+
     app.use(express.json());
-
-
-    app.use(bodyParser.urlencoded({ extended: true }));
-    module.exports = app;
-
-
-    
+    app.use(express.urlencoded({extended:true}))
